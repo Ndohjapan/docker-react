dd363e16741dc9b85a7caec64528872e260d255a
Joel <ndohjoelmbj16@gmail.com>
2023-01-19 08:04:53 +0100

    initial commit

---
+++ dockerfile
@@ -0,0 +1,12 @@
+from node:14-alpine as builder
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
+
+
--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>learn react</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          don't worry about me 
         </a>
       </header>
     </div>
